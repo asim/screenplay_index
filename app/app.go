62488ec44a567126cc4130f37a7703783f4298f7
Asim <asim@chuhnk.me>
2013-12-26 19:15:43 +0000

    Add user agent to logline

--- app/app.go
+++ app/app.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Logger(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.Header.Get("X-Forwarded-For"), r.RemoteAddr, r.Method, r.RequestURI)
+	log.Println(r.Header.Get("X-Forwarded-For"), r.RemoteAddr, r.Method, r.RequestURI, r.UserAgent())
 }
 
 func Run(host string) {
