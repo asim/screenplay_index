20e698c6ee4373240a097840d5fd2f7e4392a4df
Asim <asim@chuhnk.me>
2013-12-28 14:05:20 +0000

    metatron tags

--- static/views/layout.m
+++ static/views/layout.m
@@ -1,6 +1,8 @@
 <html>
   <head>
     <meta name="viewport" content="initial-scale=1">
+    <meta name="description" content="A screenplay index">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Got Scripts?</title>
     <style type="text/css">
       html {
