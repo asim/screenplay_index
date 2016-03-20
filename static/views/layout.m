d5f78ad8e9084e507660f3cd05dc4818e9b78b25
Asim <asim@chuhnk.me>
2016-03-20 23:25:59 +0000

    We like this

--- static/views/layout.m
+++ static/views/layout.m
@@ -59,13 +59,13 @@
       {{/alert}}
 
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-10 col-md-offset-1">
           {{{content}}}
         </div>
       </div>
 
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-10 col-md-offset-1">
           {{> _pager.m}}
         </div>
       </div>
