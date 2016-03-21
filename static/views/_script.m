dd5d6983e030435bf45d90b73329bf800f3ce24d
Asim <asim@chuhnk.me>
2016-03-21 00:10:31 +0000

    add share links

--- static/views/_script.m
+++ static/views/_script.m
@@ -8,8 +8,35 @@
   <small>{{Meta}}</small>
 </p>
 
-<div>
-  <span><a href="/s/{{Short}}" onclick="shareLink(this); return false;" style="font-size:0.8em">share</a></span>
-  <span><input type="text" value="foo" style="display: none;"></span>
-</div>
+<ul class="list-inline">
+  <li>
+    <a href="http://twitter.com/share?text="{{Title}} via @scridx"&amp;url=http://scridx.com/s/{{Short}}"
+        onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;">
+        <i class="fa fa-twitter"></i>
+        <span class="hidden">Twitter</span>
+    </a>
+  <li>
+  </li>
+    <a href="https://www.facebook.com/sharer/sharer.php?u=http://scridx.com/s/{{Short}}"
+        onclick="window.open(this.href, 'facebook-share','width=580,height=296');return false;">
+        <i class="fa fa-facebook-official"></i>
+        <span class="hidden">Facebook</span>
+    </a>
+  <li>
+  </li>
+    <a href="https://plus.google.com/share?url=http://scridx.com/s/{{Short}}"
+       onclick="window.open(this.href, 'google-plus-share', 'width=490,height=530');return false;">
+        <i class="fa fa-google-plus"></i>
+        <span class="hidden">Google+</span>
+    </a>
+  </li>
+  <li>
+    <a href="/s/{{Short}}" onclick="shareLink(this); return false;" style="font-size:0.8em">
+      <i class="fa fa-clipboard"></i>
+    </a>
+    &nbsp;
+    <span><input type="text" value="foo" style="display: none;"></span>
+  </li>
+</ul>
+
 
--- static/views/layout.m
+++ static/views/layout.m
@@ -5,6 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Scridx - A Screenplay Index</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
     <style>
       .center {
         margin-top: 100px;
